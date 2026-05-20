b4f6726918dcd09f6bbc285445e102dbd0cf326d
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-20 16:15:59 +0530

    files type and form tags

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def dashboard():
             st.write(choosedFile.name)
             st.write(choosedFile.type)
 
+        if "image" in choosedFile.type:
+            st.image(choosedFile)
+        elif "video" in choosedFile.type:
+            st.video(choosedFile)
+        elif "audio" in choosedFile.type:
+            st.audio(choosedFile)            
+
 
 def login_function():
     st.header("Login")
