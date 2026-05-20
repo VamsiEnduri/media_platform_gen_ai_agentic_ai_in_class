759330307a848b1843fdddd912db8d8f49638c4d
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-20 16:06:12 +0530

    signup and login and dashboard code updated

--- main.py
+++ main.py
@@ -2,32 +2,30 @@ import streamlit as st
 from db_c import conn_obj,cursor_obj
 st.title("Media Platform")
 
-login,signup = st.tabs(
-    ["Login","SignUp"]
-)
+if "user" not in st.session_state:
+    st.session_state.user = None
 
-cursor_obj.execute("show databases")
-dbs=cursor_obj.fetchall()
+def dashboard():
+    st.sidebar.success("welcome user")
+    st.header("dashboard")    
 
-for db in dbs:
-    st.write(db)
-
-cursor_obj.execute("show tables")
-dbs=cursor_obj.fetchall() 
-
-
-for db in dbs:
-    st.write(db)
-
-
-with login:
+def login_function():
     st.header("Login")
     with st.form("Login_Form"):
         email = st.text_input("Email")
         password = st.text_input("Password",type="password")
         btn=st.form_submit_button("Login")
 
-with signup:
+        if btn :
+            query="select * from users3 where email=%s and password = %s"
+            values=(email,password)
+            cursor_obj.execute(query,values)
+            loggedin_user=cursor_obj.fetchone()
+            st.session_state.user = loggedin_user
+            st.write("loggedin succesfully")
+            st.rerun()
+
+def signup_function():
     st.header("SignUp")
 
     with st.form("SignUp_Form"):
@@ -45,3 +43,14 @@ with signup:
             st.write("user added successfully ")
 
 
+if st.session_state.user == None:
+    login,signup = st.tabs(
+    ["Login","SignUp"]
+    )
+    with signup:
+        signup_function()
+
+    with login:
+        login_function()    
+else:
+    dashboard()
